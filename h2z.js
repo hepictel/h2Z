eca13300468b77075b501f3ebeca2e5d8b5e7643
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-07-20 16:29:55 +0200

    Update h2z.js

--- h2z.js
+++ h2z.js
@@ -29,11 +29,12 @@ const options = {
 let messages = new LRUCache(options);
 
 function middleware(data) {
-  // Root Span
+  
+  var traceId = hashString(data.callid);
   var trace = [{
    "id": data.uuid.split('-')[0] || "1234",
-   "traceId": data.call_id || "d6e9329d67b6146b",
-   "timestamp": data.cdr_start * 1000 || microtime.now(),
+   "traceId": traceId || "d6e9329d67b6146b",
+   "timestamp": data.micro_ts || microtime.now(),
    "duration": data.duration * 1000000 || 1000,
    "name": `${data.from_user} -> ${data.ruri_user}: ${data.status_text}`,
    "tags": data,
@@ -42,15 +43,15 @@ function middleware(data) {
     }
   }]
   
-  // Sub Spans
-  if (data.cdr_start){
+  // Sub Span Generator
+  if (data.micro_ts > 0){
     if (data.cdr_ringing > 0) {
         trace.push({
            "id": data.uuid.split('-')[0] + "1",
            "parentId": data.uuid.split('-')[0],
-           "traceId": data.call_id,
+           "traceId": traceId,
            "timestamp": data.cdr_start * 1000 || microtime.now(),
-           "duration": (data.cdr_ringing - data.cdr_start) * 1000000 || 1000,
+           "duration": (data.cdr_ringing * 1000) - data.micro_ts || 1000,
            "name": `${data.from_user} -> ${data.ruri_user}: Ringing`,
            "tags": data,
             "localEndpoint": {
@@ -62,9 +63,9 @@ function middleware(data) {
         trace.push({
            "id": data.uuid.split('-')[0] + "2",
            "parentId": data.uuid.split('-')[0],
-           "traceId": data.call_id,
+           "traceId": traceId,
            "timestamp": data.cdr_ringing * 1000 || microtime.now(),
-           "duration": (data.cdr_connected - data.cdr_ringing) * 1000000 || 1000,
+           "duration": (data.cdr_connected * 1000 ) - data.micro_ts || 1000,
            "name": `${data.from_user} -> ${data.ruri_user}: Connected`,
            "tags": data,
             "localEndpoint": {
@@ -76,6 +77,13 @@ function middleware(data) {
   }
 
   return trace;
+}
+
+function hashString(str) {
+    const hash = crypto.createHash('sha256');
+    hash.update(str.toString());
+    const fullHash = hash.digest('hex');
+    return fullHash.substr(0, 32);
 }
 
 const wss = new WebSocketServer({ port: process.env.WS_PORT | 18910 });
